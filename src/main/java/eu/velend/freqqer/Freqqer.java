17cef51e68d611aa376734367b381796ca18412d
eetnaviation <117541874+eetnaviation@users.noreply.github.com>
2024-07-05 23:48:36 +0300

    RELEASE INITAL - V1.1 - METEOR PROXY NOT SUPPORTED

---
+++ src/main/java/eu/velend/freqqer/Freqqer.java
@@ -0,0 +1,13 @@
+package eu.velend.freqqer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Freqqer implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
